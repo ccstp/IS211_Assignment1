b1dca2de9efd9dd3f0832e5226596635f204f0b0
Cindy Cheng <cindy.cheng94@spsmail.cuny.edu>
2021-09-06 02:08:36 -0500

    commit completed part 2 of assignment

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,30 @@
+# ASSIGNMENT 1_PART 02
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 
 class Book:
+    """
+    Class used to represent a Book
+
+    Attributes:
+    author (str): The name of the author
+    title (str): The title of the book
+    """
     def __init__(self, author, title):
-        pass
-    
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        """
+        This function prints out a formatting string displaying the book's title and author.
+        """
+        print(f'{self.title}, written by {self.author}')
 
 
 if __name__ == "__main__":
-    pass
+    book_one = Book("John Steinbeck", "Of Mice and Men")
+    book_two = Book("Harper Lee", "To Kill a Mockingbird")
+    book_one.display()
+    book_two.display()
